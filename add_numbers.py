8f2fe8e03a39302d432d8a0a22a6b4931e9426fc
praveensachdeva <50181187+praveensachdeva@users.noreply.github.com>
2023-01-06 13:57:13 -0800

    Create add_numbers.py

---
+++ add_numbers.py
@@ -0,0 +1,11 @@
+# This program adds two numbers
+
+num1 = 3.9
+num2 = 5.6
+num3 = 3.3
+
+# Add two numbers
+sum = num1 + num2 + num3
+
+# Display the sum
+print('The sum of {0} and {1} and {2} is {3}'.format(num1, num2, num3, sum))
